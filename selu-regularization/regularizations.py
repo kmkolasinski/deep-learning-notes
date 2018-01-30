3d086f14bb187c8409798e715af1a814348febbc
Krzysztof Kolasinski <kmkolasinski@gmail.com>
2018-01-30 08:15:48 +0100

    Update regularizations.py

--- selu-regularization/regularizations.py
+++ selu-regularization/regularizations.py
@@ -1,12 +1,20 @@
+# inspired by loss of VAEs
+def fc_selu_reg(x, mu):
+    # average over filter size
+    mean = K.mean(x, axis=0)
+    tau_sqr = K.mean(K.square(x), axis=0)
+    # average over batch size
+    mean_loss = K.mean(K.square(mean))
+    tau_loss = K.mean(tau_sqr - K.log(tau_sqr + K.epsilon()))
+    return mu * (mean_loss + tau_loss)    
+
+
 class SeluDenseRegularizer(Regularizer):
     def __init__(self, mu=0.001):
         self.mu = K.cast_to_floatx(mu)
 
     def __call__(self, x):
-        mean_loss = K.mean(K.square(K.sum(x, axis=0)))
-        tau_loss = - K.mean(K.log(K.sum(K.square(x), axis=0) + K.epsilon()))
-
-        return self.mu * (mean_loss + tau_loss)
+        return fc_selu_reg(x, self.mu) 
 
     def get_config(self):
         return {'mu': float(self.mu)}
@@ -21,10 +29,7 @@ class SeluConv2DRegularizer(Regularizer):
         shape = K.int_shape(x)
         num_filters = shape[-1]
         x = K.reshape(x, shape=[-1, num_filters])
-        mean_loss = K.mean(K.square(K.sum(x, axis=0)))
-        tau_loss = - K.mean(K.log(K.sum(K.square(x), axis=0) + K.epsilon()))
-
-        return self.mu * (mean_loss + tau_loss)
+        return fc_selu_reg(x, self.mu) 
 
     def get_config(self):
         return {'mu': float(self.mu)}
