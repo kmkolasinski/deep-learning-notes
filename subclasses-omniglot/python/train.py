78355898f23a773bf2b1cc44e4fb3d4859bd8e8b
kmkolasinski <kmkolasinski@gmail.com>
2018-04-07 10:35:56 +0200

    asd

--- subclasses-omniglot/python/train.py
+++ subclasses-omniglot/python/train.py
@@ -58,30 +58,33 @@ run_config = tf.estimator.RunConfig().replace(
     save_checkpoints_secs=1e9,
     session_config=session_config)
 
-classifier = tf.estimator.Estimator(
-    model_fn=model_func,
-    model_dir=model_dir,
-    config=run_config,
-    params={
-        'num_classes': num_classes,
-        'image_shape': image_shape
-    }
-)
+
 
 for epoch in tqdm(range(train_epochs)):
     print(f'Starting a training epoch [{epoch}/{train_epochs}]')
 
     train_hooks = [
         hooks.get_examples_per_second_hook(batch_size=batch_size),
-        hooks.get_profiler_hook(),
+        #hooks.get_profiler_hook(),
         hooks.get_logging_tensor_hook(tensors_to_log=[
             'head_accuracy', 'cross_entropy', 'learning_rate'])
     ]
+
+    classifier = tf.estimator.Estimator(
+        model_fn=model_func,
+        model_dir=model_dir,
+        config=run_config,
+        params={
+            'num_classes': num_classes,
+            'image_shape': image_shape
+        }
+    )
 
     classifier.train(
         input_fn=train_input_func,
         hooks=train_hooks,
         max_steps=steps_per_epoch)
+
 
     print('Starting to evaluate')
 
