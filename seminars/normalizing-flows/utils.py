09d003e9146de3f2fc455b763469099b03a06cbe
kmkolasinski <kmkolasinski@gmail.com>
2018-10-05 17:16:48 +0200

    Add celeba notebook

--- seminars/normalizing-flows/utils.py
+++ seminars/normalizing-flows/utils.py
@@ -1,10 +1,12 @@
 """Utility functions used in notebooks"""
 from collections import defaultdict
-from typing import Optional, Callable
+from pathlib import Path
+from typing import Optional, Callable, List
 
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from tensorflow import gfile
 from tqdm import tqdm
 
 
@@ -32,6 +34,50 @@ def numpy_array_to_dataset(
         )
     dataset = dataset.prefetch(4)
     return dataset
+
+
+def create_tfrecord_dataset_iterator(
+    tfrecord_paths: List[str],
+    image_size: int = 48,
+    batch_size: int = 16,
+    buffer_size: int = 512,
+    num_parallel_batches: int = 16,
+) -> tf.Tensor:
+
+    dataset = tf.data.TFRecordDataset(tfrecord_paths)
+
+    def preprocess_images(example):
+        features = tf.parse_single_example(
+            example, features={"image_raw": tf.FixedLenFeature([], tf.string)}
+        )
+
+        image = tf.decode_raw(features["image_raw"], tf.uint8)
+        image.set_shape([218 * 178 * 3])  # 218, 178
+        image = tf.cast(image, tf.float32)
+        image = tf.reshape(image, [218, 178, 3])
+        image = image[40:188, 15:163, :]
+        image = tf.reshape(image, [148, 148, 3])
+        return image
+
+    dataset = dataset.apply(
+        tf.contrib.data.shuffle_and_repeat(buffer_size=buffer_size, count=-1)
+    )
+    dataset = dataset.apply(
+        tf.contrib.data.map_and_batch(
+            map_func=preprocess_images,
+            batch_size=batch_size,
+            num_parallel_batches=num_parallel_batches,
+            drop_remainder=True,
+        )
+    )
+    dataset = dataset.prefetch(4)
+    images = dataset.make_one_shot_iterator().get_next()
+
+    x_in = tf.reshape(images, [batch_size, 148, 148, 3])
+    x_in = tf.image.resize_images(
+        x_in, [image_size, image_size], method=0, align_corners=False
+    )
+    return x_in / 255.0
 
 
 _epsilon = 1e-5
