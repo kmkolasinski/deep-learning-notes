e82f9806a04d839fb413e4cb2f67c3149b5ff3f0
kmkolasinski <kmkolasinski@gmail.com>
2019-12-05 22:13:57 +0100

    Add first working test

--- 2019-12-SuperGlue/superglue/losses.py
+++ 2019-12-SuperGlue/superglue/losses.py
@@ -1,9 +1,11 @@
+from pprint import pprint
+
 import tensorflow as tf
 import numpy as np
 
 
 def py_compute_optimal_transport(
-    M: np.ndarray, r: np.ndarray, c: np.ndarray, lam: float, epsilon=1e-5
+    M: np.ndarray, r: np.ndarray, c: np.ndarray, lam: float, epsilon=1e-6
 ):
     """
     Computes the optimal transport matrix and Slinkhorn distance using the
@@ -41,11 +43,11 @@ def sinkhorn_step(Pij: tf.Tensor, a_vec: tf.Tensor, b_vec: tf.Tensor) -> tf.Tens
     a_vec = tf.cast(a_vec, dtype=Pij.dtype)
     b_vec = tf.cast(b_vec, dtype=Pij.dtype)
 
-    row_sum = tf.reduce_sum(Pij, axis=1)  # (batch_size, num_keypoints)
     col_sum = tf.reduce_sum(Pij, axis=2)  # (batch_size, num_keypoints)
-
     u = tf.reshape(a_vec / col_sum, [batch_size, -1, 1])
     Pij = Pij * u
+
+    row_sum = tf.reduce_sum(Pij, axis=1)  # (batch_size, num_keypoints)
     v = tf.reshape(b_vec / row_sum, [batch_size, 1, -1])
     Pij = Pij * v
     return Pij
@@ -77,41 +79,52 @@ def batch_compute_optimal_transport(
 
     P = tf.exp(-lam * M)
     P = normalize_pij(P)
-
     for i in range(num_steps):
         P = sinkhorn_step(P, r, c)
-
     return P, tf.reduce_sum(P * M, axis=[1, 2])
 
 
-class SuperGlueSinkhornKnoppLayer(tf.keras.layers.Layer):
-    def __init__(self, lam: float = 5.0, num_steps: int = 100, dustbin_init = 0.0):
-        super(SuperGlueSinkhornKnoppLayer, self).__init__()
+def assignment_log_likelihood(Pij, matches):
+    matches = tf.convert_to_tensor(matches)
+    batch_size, num_keypoints, num_keypoint_b = Pij.shape.as_list()
+    assert num_keypoints == num_keypoint_b, "Assumption assertion"
+    assert matches.shape.as_list()[-1] == 2
+    assert matches.dtype in [tf.int32, tf.int64]
+
+    dustbin_index_match = tf.ones_like(matches) * (num_keypoints - 1)
+    matches = tf.where(tf.equal(matches, -1), x=dustbin_index_match, y=matches)
+
+    pij_matches = tf.gather_nd(Pij, matches, batch_dims=1)
+    pij_matches = tf.clip_by_value(pij_matches, 1e-7, 1.0)
+    loss = - tf.math.log(pij_matches)
+    loss = tf.reduce_mean(loss, -1)
+    return tf.reduce_mean(loss)
+
+
+def assignment_log_likelihood_from_matrix(pij_labels, pij_predicted):
+    pij = pij_predicted
+    pij = tf.clip_by_value(pij, 1e-7, 1.0)
+    log_loss = - pij_labels * tf.math.log(pij)
+    log_loss = tf.reduce_sum(log_loss, axis=[1, 2])
+    return tf.reduce_mean(log_loss)
+
+
+class SinkhornKnoppLayer(tf.keras.layers.Layer):
+    def __init__(self, lam: float = 5.0, num_steps: int = 100):
+        super(SinkhornKnoppLayer, self).__init__()
         self.lam = lam
         self.num_steps = num_steps
-        self.dustbin_variable = tf.Variable(dustbin_init)
 
-    def call(self, fA, fB):
-        batch_size, num_keypoints, latent_size = fA.shape.as_list()
-        assert fA.shape == fB.shape
+    def normalize_features(self, features):
+        return tf.math.l2_normalize(features, axis=-1)
 
-        fA = tf.math.l2_normalize(fA, axis=-1)
-        fB = tf.math.l2_normalize(fB, axis=-1)
-
+    def compute_score_matrix(self, fA, fB):
         # (batch_size, num_keypoints, num_keypoints)
         Sij = tf.matmul(fA, fB, transpose_b=True)
+        return Sij
 
-        row = self.dustbin_variable * tf.ones([batch_size, 1, num_keypoints])
-        col = self.dustbin_variable * tf.ones([batch_size, num_keypoints + 1, 1])
-        Sij_aug = tf.concat([Sij, row], axis=1)
-        Sij_aug = tf.concat([Sij_aug, col], axis=2)  # [batch_size, nk +1, nk + 1]
-
-        a_vec = np.array([[1.0] * num_keypoints + [num_keypoints]])
-        b_vec = np.array([[1.0] * num_keypoints + [num_keypoints]])
-        a_vec = tf.convert_to_tensor(a_vec)
-        b_vec = tf.convert_to_tensor(b_vec)
-
-        Cij = - Sij_aug # convert scores matrix to cost matrix
+    def compute_optimal_transport(self, Sij, a_vec, b_vec):
+        Cij = - Sij  # convert scores matrix to cost matrix
         Cij = tf.clip_by_value(Cij, -15 / self.lam, +15 / self.lam)
         # solve optimal transport using python solver to find good initial conditions
         # for the graph based approach.
@@ -122,10 +135,42 @@ class SuperGlueSinkhornKnoppLayer(tf.keras.layers.Layer):
         # need only one iteration to reach convergence
         alpha_beta = Pij_optimal * tf.exp(self.lam * Cij)
         alpha_beta = tf.stop_gradient(alpha_beta)
+        Pij_0 = alpha_beta * tf.exp(- self.lam * Cij)
+        Pij_0 = normalize_pij(Pij_0)
+        return sinkhorn_step(Pij_0, a_vec, b_vec)
 
-        Pij_0 = alpha_beta * tf.exp( - self.lam * Cij)
-        # Pij_0 = normalize_pij(Pij_0)
-        # Pij = sinkhorn_step(Pij_0, a_vec, b_vec)
-        Pij = sinkhorn_step(Pij_optimal, a_vec, b_vec)
-        print(Pij_optimal[0] - Pij[0])
-        return Pij_optimal#Pij
+    def call(self, fA, fB):
+        _, num_keypoints, _ = fA.shape.as_list()
+        assert fA.shape == fB.shape
+        fA = self.normalize_features(fA)
+        fB = self.normalize_features(fB)
+        # (batch_size, num_keypoints, num_keypoints)
+        Sij = self.compute_score_matrix(fA, fB)
+        a_vec = tf.ones([num_keypoints])
+        b_vec = tf.ones([num_keypoints])
+        return self.compute_optimal_transport(Sij, a_vec, b_vec), Sij
+
+
+class AugmentedSinkhornKnoppLayer(SinkhornKnoppLayer):
+    def __init__(self, lam: float = 5.0, num_steps: int = 100, dustbin_init = 0.0):
+        super(AugmentedSinkhornKnoppLayer, self).__init__(lam=lam, num_steps=num_steps)
+        self.dustbin_variable = tf.Variable(dustbin_init)
+
+    def call(self, fA, fB):
+        _, num_keypoints, latent_size = fA.shape.as_list()
+        batch_size = tf.shape(fA)[0]
+        assert fA.shape[1:] == fB.shape[1:]
+        fA = self.normalize_features(fA)
+        fB = self.normalize_features(fB)
+        Sij = self.compute_score_matrix(fA, fB)
+
+        row = self.dustbin_variable * tf.ones([batch_size, 1, num_keypoints])
+        col = self.dustbin_variable * tf.ones([batch_size, num_keypoints + 1, 1])
+        Sij_aug = tf.concat([Sij, row], axis=1)
+        Sij_aug = tf.concat([Sij_aug, col], axis=2)  # [batch_size, nk +1, nk + 1]
+
+        a_vec = np.array([[1.0] * num_keypoints + [num_keypoints]])
+        b_vec = np.array([[1.0] * num_keypoints + [num_keypoints]])
+        a_vec = tf.convert_to_tensor(a_vec)
+        b_vec = tf.convert_to_tensor(b_vec)
+        return self.compute_optimal_transport(Sij_aug, a_vec, b_vec), Sij
--- 2019-12-SuperGlue/superglue/test_losses.py
+++ 2019-12-SuperGlue/superglue/test_losses.py
@@ -28,30 +28,40 @@ class TestSinkhornKnoppLayer(tf.test.TestCase):
         for i in range(200):
             P = losses.sinkhorn_step(P, a, b)
         P = P.numpy()
-        self.assertAllClose(P[0].sum(1), a[0].numpy() / 10)
-        self.assertAllClose(P[0].sum(0), b[0].numpy() / 20)
+
+        self.assertAllClose(P[0].sum(0), b[0].numpy())
         self.assertAllClose(P[0], P[1])
         self.assertAllClose(P[0], P[2])
 
     def test_batch_compute_optimal_transport(self):
         lam = 1
-        M = tf.ones([3, 5, 5])
+        M = tf.random.normal([1, 5, 5])
         a = tf.constant([[1, 1, 1, 1, 6]], dtype=tf.float32)
         b = tf.constant([[1, 1, 1, 1, 16]], dtype=tf.float32)
         P, dist = losses.batch_compute_optimal_transport(
-            M, a, b, lam=lam, num_steps=201
+            M, a, b, lam=lam, num_steps=100
         )
-        P1 = losses.sinkhorn_step(P, a, b)
-        print(P1)
-        P = P.numpy()
-
-        self.assertAllClose(dist, [1.0, 1.0, 1.0])
-        self.assertAllClose(P[0].sum(1), a[0].numpy() / 10)
-        self.assertAllClose(P[0].sum(0), b[0].numpy() / 20)
-        self.assertAllClose(P[0], P[1])
-        self.assertAllClose(P[0], P[2])
+        Pnp, _ = losses.py_compute_optimal_transport(
+            M[0].numpy(), a[0].numpy(), b[0].numpy(), lam=lam
+        )
+        self.assertAllClose(Pnp, P[0].numpy())
+        # converged solution should not change with one additional step
+        Pnext = losses.sinkhorn_step(P, a, b)
+        self.assertAllClose(P.numpy(), Pnext.numpy())
 
     def test_sinkhorn_knopp_layer(self):
+
+        sk_layer = losses.SinkhornKnoppLayer(lam=5.0, num_steps=200)
+        fA = tf.random.normal([3, 5, 7])
+        fB = tf.random.normal([3, 5, 7])
+        Pij, Sij = sk_layer(fA, fB)
+        self.assertEqual(Pij.shape, [3, 5, 5])
+        a_vec = np.ones([5])
+        b_vec = np.ones([5])
+        Pij_np, _ = losses.py_compute_optimal_transport(- Sij[0].numpy(), a_vec, b_vec, lam=5)
+        self.assertAllClose(Pij_np, Pij[0].numpy(), atol=1e-6)
+
+    def test_super_glue_sinkhorn_knopp_layer(self):
 
         sk_layer = losses.SuperGlueSinkhornKnoppLayer(lam=5.0, num_steps=200)
 
@@ -59,14 +69,17 @@ class TestSinkhornKnoppLayer(tf.test.TestCase):
             sA = tf.random.normal([3, 5, 7])
             sB = tf.random.normal([3, 5, 7])
             tape.watch([sA, sB])
-            Pij = sk_layer(sA, sB)
+            Pij, _ = sk_layer(sA, sB)
             loss = - tf.math.log(Pij[:, 0, 0])
             gradients = tape.gradient(loss, [sk_layer.weights[0], sA, sB])
+
+        self.assertAllEqual(np.abs(gradients[1].numpy()) > 0, np.ones_like(sA))
+        self.assertAllEqual(np.abs(gradients[2].numpy()) > 0, np.ones_like(sB))
         Pij = Pij.numpy()
-        print(Pij[0].sum(0))
+        self.assertAllClose(Pij[0].sum(0), [1, 1, 1, 1, 1, 5])
         self.assertEqual(Pij.shape, (3, 6, 6))
 
-    def test_sinkhorn_knopp_layer_static(self):
+    def test_super_glue_sinkhorn_knopp_layer_static(self):
         sk_layer = losses.SuperGlueSinkhornKnoppLayer(lam=5.0, num_steps=100)
 
         @tf.function
@@ -75,13 +88,11 @@ class TestSinkhornKnoppLayer(tf.test.TestCase):
                 sA = tf.random.normal([3, 5, 7])
                 sB = tf.random.normal([3, 5, 7])
                 tape.watch([sA, sB])
-                Pij = sk_layer(sA, sB)
+                Pij, _ = sk_layer(sA, sB)
                 loss = - tf.math.log(Pij[:, 0, 0])
                 gradients = tape.gradient(loss, sk_layer.weights)
             return loss, gradients
 
         loss, grads = step()
-        print(loss)
         loss, grads = step()
-        print(loss)
 
